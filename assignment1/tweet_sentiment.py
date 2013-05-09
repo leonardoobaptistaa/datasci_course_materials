3ede1d5f6971e5fc3f672b8d8e00fcbf6646c91a
Leonardo Baptista <leonardoobaptistaa@gmail.com>
2013-05-09 11:44:27 -0300

    Finishing problem 2 / 6 - tweet_sentiment.py
    tweet_sentiment.py now have hw() function implemented to check sentiments of
    tweets. For each line in output.txt, it prints sentiment score to STDOUT.
    
    Scores are just numbers. Positive scores means good sentiment and negative scores
    means sad sentiment.

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,7 +1,29 @@
 import sys
+import json
 
 def hw():
-    print 'Hello, world!'
+    afinnfile = open("AFINN-111.txt")
+    scores = {}
+    for line in afinnfile:
+        term, score  = line.split("\t")
+        scores[term] = int(score)
+
+    output = open("output.txt")
+    for line in output:
+        j = json.loads(line)
+        text = ""
+        if ("text" in j) :
+            text = j['text']
+
+        count = 0
+        for word in text.split(' '):
+            if (word in scores) == False:
+                continue
+
+            count += scores[word]
+
+        print float(count)
+
 
 def lines(fp):
     print str(len(fp.readlines()))
@@ -10,8 +32,6 @@ def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
     hw()
-    lines(sent_file)
-    lines(tweet_file)
 
 if __name__ == '__main__':
     main()
