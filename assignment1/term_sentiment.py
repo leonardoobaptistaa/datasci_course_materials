d838786f63fa3bad8c4b75bf56bdf5f1a15e7f2f
Leonardo Baptista <leonardoobaptistaa@gmail.com>
2013-05-11 13:00:14 -0300

    Problem 3 / 6 - first submission
    Terms that aren't in main sentiments file now have scores.

--- assignment1/term_sentiment.py
+++ assignment1/term_sentiment.py
@@ -1,17 +1,42 @@
 import sys
-
-def hw():
-    print 'Hello, world!'
-
-def lines(fp):
-    print str(len(fp.readlines()))
+import json
 
 def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    
+    afinnfile = open(sys.argv[1])
+    output = open(sys.argv[2])
+    scores = {}
+    new_scores = {}
+
+    for line in afinnfile:
+        term, score  = line.split("\t")
+        scores[term] = int(score)
+
+    for line in output:
+        j = json.loads(line)
+        text = ""
+        if ("text" in j):
+            text = j['text']
+
+        text_score = 0
+        for word in text.split(' '):
+            if (word in scores) == False:
+                continue
+
+            text_score += scores[word]
+
+        for word in text.split(' '):
+            if (word in scores) == True:
+                continue
+
+            if (word in new_scores) == False:
+                new_scores[word] = 0
+
+            new_scores[word] += text_score
+
+    for word in new_scores.keys():
+            print word, float(new_scores[word])
+
 
 if __name__ == '__main__':
     main()
